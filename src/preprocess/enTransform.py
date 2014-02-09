f07c7d2edb25889314b68d21c372789da6f98113
pradeep <pradeep@hobbes.(none)>
2014-02-09 12:57:07 -0500

    Fixed bug in regex

--- src/preprocess/enTransform.py
+++ src/preprocess/enTransform.py
@@ -6,11 +6,13 @@ import re
 
 EN_TAMIL_DICT = {} # Dict to keep track to tamil to English Transliteratiosn !
 PHONE_RE = None
+PHONE_RE_STR=""
 def initializeDict(mapFile):
     """
     Initialize the dict from the input File !
     """
     global PHONE_RE
+    global PHONE_RE_STR
     PHONE_RE_STR="" # Regex to match Tamil Letters !!
     
     mapFile = open(mapFile,"rb")
@@ -22,11 +24,13 @@ def initializeDict(mapFile):
         EN_TAMIL_DICT[k] = v
 
     # Set up Regexes !! Decode 
-    for each in EN_TAMIL_DICT.keys():
-        uEach = each
+    keys = EN_TAMIL_DICT.keys()
+    keys.sort(key=len, reverse=True)
+    for each in keys:
+        uEach = each            
         PHONE_RE_STR = PHONE_RE_STR + uEach +"|"
 
-    
+    PHONE_RE_STR = PHONE_RE_STR[0:-1]
     PHONE_RE = re.compile(PHONE_RE_STR,re.UNICODE)        
 
 def getTransliteration(tamilSent):
@@ -35,11 +39,14 @@ def getTransliteration(tamilSent):
     Must match 
     """
     Usent  = tamilSent.decode("UTF-8")
+    Usent = Usent.strip()
+    #ipdb.set_trace()
     matches = PHONE_RE.findall(Usent)
     matches = filter(lambda x: x, matches)
 
     op_str =""
     for each in matches:
+        each = each.strip()
         op_str=op_str+EN_TAMIL_DICT[each]
     return op_str
     
@@ -48,7 +55,7 @@ if __name__=="__main__":
     mapFile = sys.argv[1]
     initializeDict(mapFile)
     #print EN_TAMIL_DICT
-    sampleSent = "தமிழ்"
+    sampleSent = "ரு ரு"   
     print getTransliteration(sampleSent)    
 
     
